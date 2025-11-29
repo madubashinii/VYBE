1674bd79076dcfd50d7381ba675c3cc0403ae235
madubashinii <nilakshimadubashini920@gmail.com>
2025-11-29 11:42:26 +0530

    modified server.js and connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoutes from "./routes/auth.js";
+import planRoutes from "./routes/plans.js";
+import progressRoutes from "./routes/progress.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
-app.get("/", (req, res) => {
-    res.send("Backend is running!");
-});
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/plans", planRoutes);
+app.use("/api/progress", progressRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// database
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
+
+// app.get("/", (req, res) => {
+//     res.send("Backend is running!");
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
