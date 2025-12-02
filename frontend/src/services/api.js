c5693db70e9811067e896728dc6d8607e237b1c5
Nilakshi Madubashini <167696066+madubashinii@users.noreply.github.com>
2025-12-02 15:35:23 +0530

    Update API baseURL for development and production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://localhost:5000/api",
-    baseURL: "https://vybe-neon.vercel.app/api",
+    baseURL:
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:5000/api"
+            : "https://vybe-neon.vercel.app/api",
 });
 
 api.interceptors.request.use(config => {
