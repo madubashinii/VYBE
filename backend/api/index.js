179cfc7c380eff64f66a9ac2b33db1e746929809
madubashinii <nilakshimadubashini920@gmail.com>
2025-12-02 11:52:05 +0530

    move server.js to api  folder and rename it to index.js

---
+++ backend/api/index.js
@@ -0,0 +1,25 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoutes from "../routes/auth.js";
+import planRoutes from "../routes/plans.js";
+import progressRoutes from "../routes/progress.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/plans", planRoutes);
+app.use("/api/progress", progressRoutes);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+export default app;
